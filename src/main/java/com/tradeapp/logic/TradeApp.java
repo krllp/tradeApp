03dd6089de2558916e850b4eb8678c5c58bd89a6
krllp <kiryllpopov@gmail.com>
2018-06-11 19:53:35 +0300

    Создан парсер сущностей команд, игроков и менеджеров

--- src/main/java/com/tradeapp/logic/TradeApp.java
+++ src/main/java/com/tradeapp/logic/TradeApp.java
@@ -1,16 +1,22 @@
 package com.tradeapp.logic;
 
 import com.tradeapp.config.AppConfig;
+import com.tradeapp.entities.TeamEntity;
 import com.tradeapp.utils.parser.AppConfigParser;
+import com.tradeapp.utils.parser.EntitiesParser;
+
+import java.util.List;
 
 public class TradeApp {
 
     private static final String CONFIG_FILENAME = "config.json";
 
     private AppConfig cfg;
+    private List<TeamEntity> teamsList;
 
     public TradeApp() {
         cfg = AppConfigParser.INSTANCE.parse(CONFIG_FILENAME);
+        teamsList = EntitiesParser.INSTANCE.parse(cfg.getPathToFile());
     }
 
     public void start() {
